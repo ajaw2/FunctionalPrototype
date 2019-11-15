361d71aa7bba9b72908e4cffb4aca686068f9f27
lxr <xinrong4@illinois.edu>
2019-11-15 16:13:23 -0600

    kkk

---
+++ app/src/main/java/com/example/myapplication/Sitters_near_me.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sitters_near_me extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sitters_near_me);
+    }
+}
