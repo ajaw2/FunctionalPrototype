7adf1a3ed51d236e93e273d9506f988519b5f3dc
yiwen <yiwenw6@illinois.edu>
2019-11-15 16:20:01 -0600

    ...

---
+++ app/src/main/java/com/example/myapplication/sitter_selection1.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sitter_selection1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sitter_selection1);
+    }
+
+
+}
