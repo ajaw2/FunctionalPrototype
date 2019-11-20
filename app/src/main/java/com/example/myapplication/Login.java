7b5bc18ac7aa60e5abe2831aec0025e19f0c28b4
jiewen4 <jiewen4@illinois.edu>
2019-11-20 15:22:33 -0600

    login

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -1,42 +1,14 @@
 package com.example.myapplication;
 
-import android.app.ActionBar;
-import android.view.MenuItem;
-import androidx.core.app.NavUtils;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.drm.DrmStore;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
-    private Button login_button;
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        login_button = (Button) findViewById(R.id.login_button);
-        login_button.setOnClickListener(Login.this);
-
-       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        return true;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.login_button) {
-            Intent intent = new Intent(Login.this, Register.class);
-            startActivity(intent);
-        }
     }
 }
