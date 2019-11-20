fc12d7638185cc54fc9f2175ff995f0e9ee8ebba
yiwen <yiwenw6@illinois.edu>
2019-11-20 15:21:38 -0600

    Additional Details page

--- app/src/main/java/com/example/myapplication/sitter_selection1.java
+++ app/src/main/java/com/example/myapplication/sitter_selection1.java
@@ -3,13 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class sitter_selection1 extends AppCompatActivity {
+public class sitter_selection1 extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sitter_selection1);
+
+
     }
 
 
