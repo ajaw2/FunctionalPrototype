d676e0da85e9a3753f77533ebfdb049909414f94
ajaw2 <ajaw2@illinois.edu>
2019-11-20 14:41:56 -0600

    added button for login

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loginButton = (Button) findViewById(R.id.login);
+
+        loginButton.setOnClickListener(this);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.login) {
+            Intent intent = new Intent(Login.this, Searching_for.class);
+            startActivity(intent);
+        }
     }
 }
