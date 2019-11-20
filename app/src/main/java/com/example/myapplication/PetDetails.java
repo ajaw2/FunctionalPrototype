4cb8b21172f4ca1ab6e35246001f26b2d528a5ac
lxr <xinrong4@illinois.edu>
2019-11-20 16:10:40 -0600

    kkk

--- app/src/main/java/com/example/myapplication/PetDetails.java
+++ app/src/main/java/com/example/myapplication/PetDetails.java
@@ -2,13 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PetDetails extends AppCompatActivity {
+public class PetDetails extends AppCompatActivity implements View.OnClickListener{
+    private Button pet_add_detail_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_details);
+
+        pet_add_detail_Button = (Button) findViewById(R.id.pet_add_detail);
+        pet_add_detail_Button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.pet_add_detail) {
+            Intent intent = new Intent(PetDetails.this, pet_info.class);
+            startActivity(intent);
+        }
     }
 }
