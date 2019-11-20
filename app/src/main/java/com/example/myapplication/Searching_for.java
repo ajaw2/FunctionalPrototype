efd4010073f05ebb2622982e94064c253eb4ba38
jiewen4 <jiewen4@illinois.edu>
2019-11-20 14:54:59 -0600

    pet_select

--- app/src/main/java/com/example/myapplication/Searching_for.java
+++ app/src/main/java/com/example/myapplication/Searching_for.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Searching_for extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Searching_for extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searching_for);
+    }
+
+    public void search_pet(View view)
+    {
+        Intent intent = new Intent(this, pet_selection.class);
+        startActivity(intent);
+    }
+
+    public void search_sitter(View view)
+    {
+        Intent intent = new Intent(this, sitter_selection1.class);
+        startActivity(intent);
     }
 }
