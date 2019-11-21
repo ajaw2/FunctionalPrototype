2c09d02cf6dc4356189cdaf5c34316aa15b5a301
lxr <xinrong4@illinois.edu>
2019-11-20 22:14:08 -0600

    add more sitters near me

--- app/src/main/java/com/example/myapplication/pet_info.java
+++ app/src/main/java/com/example/myapplication/pet_info.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class pet_info extends AppCompatActivity {
+public class pet_info extends AppCompatActivity implements View.OnClickListener {
+    private Button contact_owner_Button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_info);
 
+        contact_owner_Button = (Button) findViewById(R.id.contact_owner);
+        contact_owner_Button.setOnClickListener(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.contact_owner) {
+            Intent intent = new Intent(pet_info.this, message.class);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -21,5 +35,10 @@ public class pet_info extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), PetDetails.class);
         startActivity(intent);
         return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
