c901a255404d08114157d063b7370f2862d233e1
lxr <xinrong4@illinois.edu>
2019-11-20 22:19:01 -0600

    add back button to the message page

--- app/src/main/java/com/example/myapplication/message.java
+++ app/src/main/java/com/example/myapplication/message.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class message extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class message extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(getApplicationContext(), pet_info.class);
+        startActivity(intent);
+        return true;
     }
 }
