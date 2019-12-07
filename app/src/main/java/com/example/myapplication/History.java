13cc877f61662ce27c004898ed439a3712e9a99c
cyberroli <rsantos3@illinois.ed>
2019-12-06 19:54:14 -0600

    abnjfknbalfabhjabfljdsa

--- app/src/main/java/com/example/myapplication/History.java
+++ app/src/main/java/com/example/myapplication/History.java
@@ -2,13 +2,50 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class History extends AppCompatActivity {
+public class History extends AppCompatActivity implements View.OnClickListener{
+    private Button review_done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        review_done = (Button) findViewById(R.id.review);
+        review_done.setOnClickListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.settings_mini){
+            Intent intent = new Intent(this, AccountPage.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(getApplicationContext(), AccountPage.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.review) {
+            Intent intent = new Intent(this, comment.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/myapplication/comment.java
+++ app/src/main/java/com/example/myapplication/comment.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class comment extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class comment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.settings_mini){
+            Intent intent = new Intent(this, AccountPage.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(getApplicationContext(), History.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
